abbe33649fa37ef4c846d8c9f2782f72798c0c09
zxf <1565453341@qq.com>
2021-06-08 16:36:34 +0800

    2021/6/8 更新了注册页面

--- bootstrap_crud.sql
+++ bootstrap_crud.sql
@@ -2,7 +2,8 @@
 SQLyog Ultimate v12.09 (64 bit)
 MySQL - 5.6.11 : Database - ssm
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -41,3 +42,42 @@ insert  into `tb_user`(`id`,`username`,`password`,`email`,`truename`,`create_tim
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.6.11 : Database - ssm
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ssm` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `ssm`;
+
+/*Table structure for table `login_user` */
+
+DROP TABLE IF EXISTS `login_user`;
+
+CREATE TABLE `login_user` (
+                              `id` int(11) NOT NULL AUTO_INCREMENT,
+                              `loginName` varchar(50) DEFAULT NULL COMMENT '登录账号',
+                              `loginPwd` varchar(100) DEFAULT NULL COMMENT '登录密码',
+                              PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='用户登录表';
+
+/*Data for the table `login_user` */
+
+insert  into `login_user`(`id`,`loginName`,`loginPwd`) values (1,'zou','zou');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
